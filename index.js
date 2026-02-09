b59e6fb0bf23a493e5183bc242694f8753fdaa8e
tanishqqsharma <143253287+tanishqqsharma@users.noreply.github.com>
2026-02-09 17:26:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodeList.length; i++) {
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodeList[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var task = document.getElementById("task-input");
+var addTaskBtn = document.getElementById("add-task");
+var tasks = document.getElementById("tasks");
+
+addTaskBtn.addEventListener("click", addTask);
+function addTask() {
+    var taskText = task.value;
+    var t = document.createTextNode(taskText);
+    if (taskText !== "") {
+        var li = document.createElement("li");
+        li.appendChild(t);
+        tasks.appendChild(li);
+
+        var span = document.createElement("span");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function () {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+    }
+    else {
+        alert("you must write a task!");
+    }
+    task.value = "";
+}
+tasks.addEventListener("click", checkTask);
+function checkTask(e) {
+    if (e.target.tagName == "LI") {
+        e.target.classList.toggle("checked");
+    }
+}
